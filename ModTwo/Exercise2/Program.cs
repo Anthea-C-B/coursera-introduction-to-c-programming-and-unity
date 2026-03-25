c6bfc290d8485e6f8590cb2c07dac48d8ebaf926
Anthea <acbuckthorp@gmail.com>
2026-03-25 16:20:15 +0000

    Update with exercise 2 and 3

---
+++ ModTwo/Exercise2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+
+            // prompt for user input and read the input as integer
+
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Loading game ...");
+
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("New Game Selected");
+                    break;
+                case 2:
+                    Console.WriteLine("Load Game Selected");
+                    break;
+                case 3:
+                    Console.WriteLine("Options Selected");
+                    break;
+                case 4:
+                    Console.WriteLine("Quit Selected");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
